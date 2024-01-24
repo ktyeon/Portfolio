38cb33559ebe6e57548e8ef85a802ced760a6e67
ktyeon <84512238+ktyeon@users.noreply.github.com>
2024-01-23 22:48:50 -0500

    Edit header size has been changed depends on browser size

--- main/header.js
+++ main/header.js
@@ -1,8 +1,34 @@
-const toggleBtn = document.querySelector('.navbar__toggle');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
+// const toggleBtn = document.querySelector('.navbar__toggle');
+// const menu = document.querySelector('.navbar__menu');
+// const icons = document.querySelector('.navbar__icons');
 
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
+// toggleBtn.addEventListener('click', () => {
+//     menu.classList.toggle('active');
+//     icons.classList.toggle('active');
+// });
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     console.log('DOMContentLoaded event fired');
+//     const navbarToggle = document.querySelector('.navbar__toggle');
+//     const navbarMenu = document.querySelector('.navbar__menu');
+
+//     navbarToggle.addEventListener('click', function () {
+//         console.log('Toggle button clicked!');
+//         navbarMenu.classList.toggle('show');
+//     });
+// });
+
+  
+document.addEventListener('DOMContentLoaded', function () {
+    const navbarToggle = document.querySelector('.navbar__toggle');
+    const navbarMenu = document.querySelector('.navbar__menu');
+
+    console.log('navbarToggle:', navbarToggle);
+    console.log('navbarMenu:', navbarMenu);
+
+    navbarToggle.addEventListener('click', function () {
+        console.log('Toggle button clicked!');
+        navbarMenu.classList.toggle('show');
+    });
 });
