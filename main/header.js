4cbcdeeaf5917c824d82e1a56e012c782d9f6e6b
ktyeon <84512238+ktyeon@users.noreply.github.com>
2024-01-27 10:09:35 -0500

    delete all css file merge css to html

--- main/header.js
+++ main/header.js
@@ -1,8 +1,18 @@
-const toggleBtn = document.querySelector('.navbar__toggle');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
+// const toggleBtn = document.querySelector('.navbar__toggle');
+// const menu = document.querySelector('.navbar__menu');
+// const icons = document.querySelector('.navbar__icons');
 
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
+// toggleBtn.addEventListener('click', () => {
+//     menu.classList.toggle('active');
+//     icons.classList.toggle('active');
+// });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navbarToggle = document.querySelector('.navbar__toggle');
+    const navbarMenu = document.querySelector('.navbar__menu');
+
+    navbarToggle.addEventListener('click', function () {
+        navbarMenu.classList.toggle('show');
+    });
 });
