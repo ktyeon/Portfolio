d982411a18dc82a0f2081a99c507868a13eb9482
ktyeon <84512238+ktyeon@users.noreply.github.com>
2024-01-27 21:31:33 -0500

    Add Responsive Web

--- main/header.js
+++ main/header.js
@@ -1,18 +1,10 @@
-// const toggleBtn = document.querySelector('.navbar__toggle');
-// const menu = document.querySelector('.navbar__menu');
-// const icons = document.querySelector('.navbar__icons');
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleBtn = document.querySelector('.navbar__toggleBtn');
+    const menu = document.querySelector('.navbar__menu');
+    const icons = document.querySelector('.navbar__icons');
 
-// toggleBtn.addEventListener('click', () => {
-//     menu.classList.toggle('active');
-//     icons.classList.toggle('active');
-// });
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    const navbarToggle = document.querySelector('.navbar__toggle');
-    const navbarMenu = document.querySelector('.navbar__menu');
-
-    navbarToggle.addEventListener('click', function () {
-        navbarMenu.classList.toggle('show');
+    toggleBtn.addEventListener('click', () => {
+        menu.classList.toggle('active');
+        icons.classList.toggle('active');
     });
 });
